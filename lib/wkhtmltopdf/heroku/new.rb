f0257a4344cd584073eeaf7eb7c0b322025a3b32
Andrej Pancik <apancik@emerald-core-2.(none)>
2014-04-16 12:52:52 +0200

    Refactor

--- lib/wkhtmltopdf/heroku/new.rb
+++
@@ -1,9 +0,0 @@
-require "wkhtmltopdf/heroku/new/version"
-
-module Wkhtmltopdf
-  module Heroku
-    module New
-      # Your code goes here...
-    end
-  end
-end
--- lib/wkhtmltopdf/heroku/new/version.rb
+++
@@ -1,7 +0,0 @@
-module Wkhtmltopdf
-  module Heroku
-    module New
-      VERSION = "0.0.1"
-    end
-  end
-end
