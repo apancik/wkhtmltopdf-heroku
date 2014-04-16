03e5017b640d3d148206433b3913a422c669d5ad
Andrej Pancik <apancik@emerald-core-2.(none)>
2014-04-16 13:58:50 +0200

    Support for OSX

--- lib/wkhtmltopdf-heroku.rb
+++ lib/wkhtmltopdf-heroku.rb
@@ -1,4 +1,4 @@
-WKHTMLTOPDF_PATH = File.expand_path "../../bin/wkhtmltopdf-linux-amd64", __FILE__
+WKHTMLTOPDF_PATH = File.expand_path "../../bin/wkhtmltopdf", __FILE__
 
 begin
   require 'wicked_pdf'
